fbcceaa79558cf3cce291c76322f04f746f26eb4
Hutaba-Makura <152959976+Hutaba-Makura@users.noreply.github.com>
2025-12-22 18:26:20 +0900

    Refactor：content.js

--- content.js
+++ content.js
@@ -1,18 +1,29 @@
-  function checkAndReplaceElement() {
-    setTimeout(() => {
-      const targetAbstract = document.querySelector('.YzCcne');
-      const targetTab = document.querySelector('.olrp5b');
-      if (targetAbstract || targetTab) {
-        if(targetAbstract) targetAbstract.style.display = 'none';
-        if(targetTab) targetTab.style.display = 'none';
+  function replaceElement() {
+    const targetAbstract = document.querySelector('.YzCcne');
+    const targetTab = document.querySelector('.olrp5b');
+    if (targetAbstract || targetTab) {
+      if(targetAbstract) targetAbstract.style.display = 'none';
+      if(targetTab) targetTab.style.display = 'none';
+
+      window.isAIFreeDisplayed = true;
+    }
+  }
   
-        window.isAIFreeDisplayed = true;
-      }
-    },10);
+  function checkAndReplaceElement() {
+    for (let i = 0; i < 5; i++) {
+      setTimeout(() => {
+        replaceElement();
+      }, 50 * (i + 1));
+    }
   }
   
   // ページが読み込まれたら実行
   if (!window.isAIFreeDisplayed) {
     checkAndReplaceElement();
   }
+
+  // "document_end"のタイミングでも実行
+  document.addEventListener('DOMContentLoaded', () => {
+    checkAndReplaceElement();
+  });
   
